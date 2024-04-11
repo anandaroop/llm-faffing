3dcd34f144ff0e5bb793158ff3b5964852327a86
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2024-04-11 16:27:36 -0400

    feat: actually execute the function calls and output the result

--- src/01-function-calling/index.ts
+++ src/01-function-calling/index.ts
@@ -89,4 +89,118 @@ const response = await openai.chat.completions.create({
 });
 
 console.log(JSON.stringify(messages));
-console.log(JSON.stringify(response.choices[0]));
+console.log(JSON.stringify(response.choices[0].message));
+
+if (response.choices[0].finish_reason === "tool_calls") {
+  const name = response.choices[0].message.tool_calls?.[0].function.name;
+  const args = JSON.parse(
+    response.choices[0].message.tool_calls?.[0].function.arguments || "null"
+  );
+
+  console.log(JSON.stringify({ name, args }));
+
+  let artists;
+
+  if (name === "get_artists") {
+    artists = await get_artists(args);
+    console.log(JSON.stringify(artists));
+  }
+
+  if (name === "get_curated_artists") {
+    artists = await get_curated_artists(args);
+    console.log(JSON.stringify(artists));
+  }
+
+  if (artists) {
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      temperature: 0,
+      messages: [
+        {
+          role: "user",
+          content: `
+            Based on the following JSON context, return a nicely formatted bullet list of artists.
+            Each artist entry should contain a name, nationality, birth/death dates, as well as
+            a link to their Artsy profile in the form https://www.artsy.net/artist/<slug>.
+
+            An example entry would look like this:
+            - [Vincent van Gogh](https://www.artsy.net/artist/vincent-van-gogh) (Dutch, 1853–1890)
+
+            Make sure you use all of the items in the context.
+
+            Context
+            '''
+            ${JSON.stringify(artists, null, 2)}
+            '''
+        `,
+        },
+      ],
+    });
+
+    // console.log(JSON.stringify(response.choices[0].message));
+    console.error(response.choices[0].message.content);
+  }
+}
+
+async function metaphysics(args: {
+  query: string;
+  variables: Record<string, unknown>;
+}) {
+  const { query, variables } = args;
+
+  const url = "https://metaphysics-production.artsy.net/v2";
+  const headers = {
+    "Content-Type": "application/json",
+  };
+  const body = JSON.stringify({ query, variables });
+  const options = { method: "POST", headers, body };
+
+  const response = await fetch(url, options);
+  const json = await response.json();
+  return json;
+}
+
+async function get_artists(args: { size: number; sort: string }) {
+  const query = `query GetArtists($size: Int!, $sort: ArtistSorts) {
+    artists(size: $size, sort: $sort) {
+      slug
+      name
+      formattedNationalityAndBirthday
+      counts {
+        forSaleArtworks
+      }
+    }
+  }`;
+
+  const variables = {
+    size: args.size,
+    sort: args.sort,
+  };
+
+  const response = await metaphysics({ query, variables });
+  return response;
+}
+
+async function get_curated_artists(args: { size: number }) {
+  const query = `query GetCuratedArtists($size: Int!) {
+    curatedTrendingArtists(first: $size) {
+      edges {
+        node {
+          slug
+          name
+          formattedNationalityAndBirthday
+          counts {
+            forSaleArtworks
+          }
+        }
+      }
+    }
+  }`;
+
+  const variables = {
+    size: args.size,
+  };
+
+  const response = await metaphysics({ query, variables });
+  return response;
+}
